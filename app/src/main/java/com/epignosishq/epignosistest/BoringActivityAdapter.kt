b989ebb7d58afa4412caf8508ee7394ba88031ca
Dan Zeevi <danzeevi2@gmail.com>
2020-12-10 12:42:19 +0200

    added bind ViewHolder

--- app/src/main/java/com/epignosishq/epignosistest/BoringActivityAdapter.kt
+++ app/src/main/java/com/epignosishq/epignosistest/BoringActivityAdapter.kt
@@ -8,17 +8,20 @@ import com.epignosis.epignosistest.R
 import com.epignosishq.epignosistest.model.BoringActivity
 import kotlinx.android.synthetic.main.boring_activity_item.view.*
 
-class BoringActivityAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class BoringActivityAdapter() : RecyclerView.Adapter<BoringActivityAdapter.ActivityViewHolder>() {
     private var activities: List<BoringActivity> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityViewHolder {
-        return ActivityViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.boring_activity_item, parent, false))
+        return ActivityViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.boring_activity_item, parent, false)
+        )
     }
 
     override fun getItemCount() = activities.size
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
 
+    override fun onBindViewHolder(holder: ActivityViewHolder, position: Int) {
+            holder.bind(activities[position])
     }
 
     fun setList(activities: List<BoringActivity>) {
@@ -30,10 +33,11 @@ class BoringActivityAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
         activities.plus(activity)
         notifyItemInserted(activities.size - 1)
     }
-}
 
-class ActivityViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    fun bind(itemView: View, activity: BoringActivity) {
-        itemView.activity_name.text = activity.activity
+
+    inner class ActivityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(activity: BoringActivity) {
+            itemView.activity_name.text = activity.activity
+        }
     }
 }
